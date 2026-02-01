59566cb4f69f3aa29e40028369fa55c5f19f7813
NazarKatasonov <nazariikryt22@gmail.com>
2026-02-01 23:48:14 +0500

    init project

---
+++ dist/shop.8df19a18.js
@@ -0,0 +1,22 @@
+const products = [
+    {
+        id: 1,
+        title: "\u041D\u0430\u0432\u0443\u0448\u043D\u0438\u043A\u0438",
+        price: 1200,
+        category: "tech"
+    },
+    {
+        id: 2,
+        title: "\u0424\u0443\u0442\u0431\u043E\u043B\u043A\u0430",
+        price: 700,
+        category: "clothes"
+    },
+    {
+        id: 3,
+        title: "\u0421\u043C\u0430\u0440\u0442-\u0433\u043E\u0434\u0438\u043D\u043D\u0438\u043A",
+        price: 2500,
+        category: "tech"
+    }
+];
+
+//# sourceMappingURL=shop.8df19a18.js.map
---
+++ src/js/render.js
@@ -0,0 +1,16 @@
+const productContainer = document.querySelector('.product')
+
+export const renderItem = function(products){
+productContainer.innerHTML = ""
+
+
+products.forEach(product=>{
+    productContainer.innerHTML += `
+     <div class="product">
+        <h3>${product.title}</h3>
+        <p>${product.price}</p>
+        <button data-id="${product.id}">купити</button>
+    </div>
+    `
+})
+}
